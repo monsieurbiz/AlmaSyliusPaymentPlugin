23850d18adaa84a5a6f932283f50a3436480f67d
Olivier Lance <olivier.lance@getalma.eu>
2021-01-28 23:38:56 +0100

    fix: improve form validation so that data is not saved in case of error

--- src/Form/Type/AlmaGatewayConfigurationType.php
+++ src/Form/Type/AlmaGatewayConfigurationType.php
@@ -11,6 +11,7 @@ use Payum\Core\Bridge\Spl\ArrayObject;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormError;
@@ -24,14 +25,21 @@ final class AlmaGatewayConfigurationType extends AbstractType
 {
     /** @var TranslatorInterface */
     private $translator;
+
     /**
      * @var AlmaBridgeInterface
      */
     private $almaBridge;
+
     /**
      * @var LoggerInterface
      */
     private $logger;
+
+    /**
+     * @var array<string, string>
+     */
+    private $errors;
 
     public function __construct(
         TranslatorInterface $translator,
@@ -41,19 +49,21 @@ final class AlmaGatewayConfigurationType extends AbstractType
         $this->translator = $translator;
         $this->almaBridge = $almaBridge;
         $this->logger = $logger;
+
+        $this->errors = [];
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add(GatewayConfigInterface::CONFIG_LIVE_API_KEY, TextType::class, [
-                'label' => 'alma_sylius_payment_plugin.ui.live_api_key_label',
-                'help' => $this->translator->trans('alma_sylius_payment_plugin.ui.find_api_keys_in_dashboard'),
+                'label' => 'alma_sylius_payment_plugin.config.live_api_key_label',
+                'help' => $this->translator->trans('alma_sylius_payment_plugin.config.find_api_keys_in_dashboard'),
                 'help_html' => true,
             ])
             ->add(GatewayConfigInterface::CONFIG_TEST_API_KEY, TextType::class, [
-                'label' => 'alma_sylius_payment_plugin.ui.test_api_key_label',
-                'help' => $this->translator->trans('alma_sylius_payment_plugin.ui.find_api_keys_in_dashboard'),
+                'label' => 'alma_sylius_payment_plugin.config.test_api_key_label',
+                'help' => $this->translator->trans('alma_sylius_payment_plugin.config.find_api_keys_in_dashboard'),
                 'help_html' => true,
             ])
             ->add(
@@ -61,11 +71,11 @@ final class AlmaGatewayConfigurationType extends AbstractType
                 ChoiceType::class,
                 [
                     'choices' => [
-                        'Live' => AlmaClient::LIVE_MODE,
-                        'Test' => AlmaClient::TEST_MODE,
+                        $this->translator->trans('alma_sylius_payment_plugin.api.live_mode') => AlmaClient::LIVE_MODE,
+                        $this->translator->trans('alma_sylius_payment_plugin.api.test_mode') => AlmaClient::TEST_MODE,
                     ],
-                    'label' => $this->translator->trans('alma_sylius_payment_plugin.ui.api_mode_label'),
-                    'help' => $this->translator->trans('alma_sylius_payment_plugin.ui.api_mode_tip'),
+                    'label' => $this->translator->trans('alma_sylius_payment_plugin.config.api_mode_label'),
+                    'help' => $this->translator->trans('alma_sylius_payment_plugin.config.api_mode_tip'),
                 ]
             )
             ->add(
@@ -73,40 +83,61 @@ final class AlmaGatewayConfigurationType extends AbstractType
                 ChoiceType::class,
                 [
                     'choices' => [
-                        $this->translator->trans('alma_sylius_payment_plugin.ui.installments_count_choice_label',
-                            ['installments_count' => 2]) => 2,
-                        $this->translator->trans('alma_sylius_payment_plugin.ui.installments_count_choice_label',
-                            ['installments_count' => 3]) => 3,
-                        $this->translator->trans('alma_sylius_payment_plugin.ui.installments_count_choice_label',
-                            ['installments_count' => 4]) => 4,
+                        $this->translator->trans('alma_sylius_payment_plugin.config.installments_count_choice_label',
+                            ['%installments_count%' => 2]) => 2,
+                        $this->translator->trans('alma_sylius_payment_plugin.config.installments_count_choice_label',
+                            ['%installments_count%' => 3]) => 3,
+                        $this->translator->trans('alma_sylius_payment_plugin.config.installments_count_choice_label',
+                            ['%installments_count%' => 4]) => 4,
                     ],
-                    'label' => $this->translator->trans('alma_sylius_payment_plugin.ui.installments_count_label'),
+                    'label' => $this->translator->trans('alma_sylius_payment_plugin.config.installments_count_label'),
                 ]
-            );
+            )
+            ->add(GatewayConfigInterface::CONFIG_MERCHANT_ID, HiddenType::class);
 
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event): void {
-            $data = ArrayObject::ensureArrayObject($event->getData());
-            $this->almaBridge->initialize($data);
-            $config = $this->almaBridge->getGatewayConfig();
+        $builder
+            ->addEventListener(FormEvents::PRE_SUBMIT, [$this, 'onPreSubmit'])
+            ->addEventListener(FormEvents::POST_SUBMIT, [$this, 'onPostSubmit']);
+    }
 
-            $apiKey = $config->getActiveApiKey();
-            Assert::notEmpty($apiKey);
+    public function onPreSubmit(FormEvent $event): void
+    {
+        $this->errors = [];
+        $data = ArrayObject::ensureArrayObject($event->getData());
 
-            $merchant = $this->almaBridge->getMerchantInfo();
+        // Only check the API key for the mode that's been activated by the merchant
+        $apiKeyConfigKey = $data[GatewayConfigInterface::CONFIG_API_MODE] === AlmaClient::LIVE_MODE
+            ? GatewayConfigInterface::CONFIG_LIVE_API_KEY
+            : GatewayConfigInterface::CONFIG_TEST_API_KEY;
 
-            if ($merchant == null) {
-                $apiKeyConfigKey = $config->getApiMode() === AlmaClient::LIVE_MODE
-                    ? GatewayConfigInterface::CONFIG_LIVE_API_KEY
-                    : GatewayConfigInterface::CONFIG_TEST_API_KEY;
+        /** @var string $apiKey */
+        $apiKey = $data[GatewayConfigInterface::CONFIG_API_MODE] === AlmaClient::LIVE_MODE
+            ? $data[GatewayConfigInterface::CONFIG_LIVE_API_KEY]
+            : $data[GatewayConfigInterface::CONFIG_TEST_API_KEY];;
 
-                $event->getForm()->get($apiKeyConfigKey)->addError(new FormError(
-                    $this->translator->trans('alma_sylius_payment_plugin.errors.invalid_api_key', [], 'validators')
-                ));
-            } else {
-                $updatedData = $data->getArrayCopy();
-                $updatedData[GatewayConfigInterface::CONFIG_MERCHANT_ID] = $merchant->id;
-                $event->setData($updatedData);
-            }
-        });
+        if ($apiKey == null || trim($apiKey) == "") {
+            $this->errors[$apiKeyConfigKey] = 'alma_sylius_payment_plugin.errors.empty_api_key';
+
+            return;
+        }
+
+        // At this point we know $data contains an API key we can try to connect with
+        $this->almaBridge->initialize($data);
+        $merchant = $this->almaBridge->getMerchantInfo();
+        if ($merchant === null) {
+            $this->errors[$apiKeyConfigKey] = 'alma_sylius_payment_plugin.errors.invalid_api_key';
+        } else {
+            $data[GatewayConfigInterface::CONFIG_MERCHANT_ID] = $merchant->id;
+            $event->setData($data->getArrayCopy());
+        }
+    }
+
+    public function onPostSubmit(FormEvent $event): void
+    {
+        $form = $event->getForm();
+
+        foreach ($this->errors as $field => $error) {
+            $form->get($field)->addError(new FormError($this->translator->trans($error)));
+        }
     }
 }
