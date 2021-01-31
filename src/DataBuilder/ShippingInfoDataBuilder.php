9cc4efbe5c3ba432575ab83819b7587c587cf73d
Olivier Lance <olivier.lance@getalma.eu>
2021-02-01 00:03:43 +0100

    feat: add a ShippingInfo data builder

---
+++ src/DataBuilder/ShippingInfoDataBuilder.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alma\SyliusPaymentPlugin\DataBuilder;
+
+
+use Doctrine\Common\Collections\Collection;
+use Sylius\Component\Core\Model\PaymentInterface;
+use Sylius\Component\Core\Model\ShipmentInterface;
+use Sylius\Component\Core\Model\ShippingMethodInterface;
+use Sylius\Component\Registry\ServiceRegistryInterface;
+use Sylius\Component\Shipping\Calculator\CalculatorInterface;
+use Sylius\Component\Shipping\Model\ShippingMethodInterface as BaseShippingMethodInterface;
+use Sylius\Component\Shipping\Resolver\ShippingMethodsResolverInterface;
+use Webmozart\Assert\Assert;
+
+class ShippingInfoDataBuilder implements DataBuilderInterface
+{
+    /**
+     * @var ShippingMethodsResolverInterface
+     */
+    private $methodsResolver;
+    /**
+     * @var ServiceRegistryInterface
+     */
+    private $calculators;
+
+    public function __construct(
+        ServiceRegistryInterface $calculators,
+        ShippingMethodsResolverInterface $methodsResolver
+    ) {
+        $this->methodsResolver = $methodsResolver;
+        $this->calculators = $calculators;
+    }
+
+    public function __invoke(array $data, PaymentInterface $payment): array
+    {
+        $order = $payment->getOrder();
+        Assert::notNull($order);
+
+        if (!$order->hasShipments()) {
+            return $data;
+        }
+
+        $shipments = $order->getShipments();
+        if ($shipments instanceof Collection) {
+            $shipments = $shipments->getValues();
+        }
+
+        /** @var ShipmentInterface[] $shipments */
+        $selectedOptions = $this->getSelectedOptions($shipments);
+        $availableOptions = $this->getAvailableOptions($shipments);
+
+        $data['payment']['shipping_info'] = [
+            'selected_options' => $selectedOptions,
+            'available_options' => $availableOptions
+        ];
+
+        return $data;
+    }
+
+    /**
+     * @param ShipmentInterface[] $shipments
+     * @return array[]
+     */
+    private function getSelectedOptions(array $shipments): array
+    {
+        return array_map(function (ShipmentInterface $shipment): array {
+            /** @var ShippingMethodInterface $method */
+            $method = $shipment->getMethod();
+            return $this->buildShippingOption($shipment, $method);
+        }, $shipments);
+    }
+
+    /**
+     * @param ShipmentInterface[] $shipments
+     * @return array
+     */
+    private function getAvailableOptions(array $shipments): array
+    {
+        $options = [];
+
+        foreach ($shipments as $shipment) {
+            if (!$this->methodsResolver->supports($shipment)) {
+                continue;
+            }
+
+            $methods = $this->methodsResolver->getSupportedMethods($shipment);
+
+            foreach ($methods as $method) {
+                $options[] = $this->buildShippingOption($shipment, $method);
+            }
+        }
+
+        return $options;
+    }
+
+    private function buildShippingOption(ShipmentInterface $shipment, BaseShippingMethodInterface $method): array
+    {
+        $nameProvider = $method->getTranslation();
+        if (array_key_exists('fr', $method->getTranslations())) {
+            $nameProvider = $method->getTranslation('fr');
+        }
+
+        /** @var CalculatorInterface $calculator */
+        $calculator = $this->calculators->get($method->getCalculator());
+
+        return [
+            'amount' => $calculator->calculate($shipment, $method->getConfiguration()),
+            'title' => $nameProvider->getDescription(),
+            'carrier' => $nameProvider->getName()
+        ];
+    }
+}
