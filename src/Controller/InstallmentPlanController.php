927315701608797dc4ecb79e1524379a9237db2b
Benjamin Freoua <benjamin.freoua@getalma.eu>
2023-09-11 16:22:44 +0200

    wip : p10/12x

--- src/Controller/InstallmentPlanController.php
+++ src/Controller/InstallmentPlanController.php
@@ -53,24 +53,9 @@ final class InstallmentPlanController
 
     public function renderPaymentPageInstallmentPlanAction(Request $request): Response
     {
-        $orderId = $request->attributes->getInt('orderId');
-        /** @var ChannelInterface $channel */
-        $channel = $this->channelContext->getChannel();
-        /** @var OrderInterface $order */
-        $order = $this->orderRepository->find($orderId);
-
         try {
-            return new Response($this->twig->render('@SyliusPayPalPlugin/payFromPaymentPage.html.twig', [
-                'available_countries' => $this->availableCountriesProvider->provide(),
-                'cancelPayPalPaymentUrl' => $this->router->generate('sylius_paypal_plugin_cancel_payment'),
-                'clientId' => $this->payPalConfigurationProvider->getClientId($channel),
-                'currency' => $order->getCurrencyCode(),
-                'completePayPalOrderFromPaymentPageUrl' => $this->router->generate('sylius_paypal_plugin_complete_paypal_order_from_payment_page', ['id' => $orderId]),
-                'createPayPalOrderFromPaymentPageUrl' => $this->router->generate('sylius_paypal_plugin_create_paypal_order_from_payment_page', ['id' => $orderId]),
-                'errorPayPalPaymentUrl' => $this->router->generate('sylius_paypal_plugin_payment_error'),
-                'locale' => $this->localeProcessor->process((string) $order->getLocaleCode()),
-                'orderId' => $orderId,
-                'partnerAttributionId' => $this->payPalConfigurationProvider->getPartnerAttributionId($channel),
+            return new Response($this->twig->render('@AlmaSyliusPaymentPlugin/installmentPlan.html.twig', [
+                'toto' => 'toto',
             ]));
         } catch (\InvalidArgumentException $exception) {
             return new Response('');
