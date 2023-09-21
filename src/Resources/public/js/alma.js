f322d0b02b343d6cd7b7b30025f20621885c78fe
Benjamin Freoua <benjamin.freoua@getalma.eu>
2023-09-21 12:04:45 +0200

    add security to execute js

--- src/Resources/public/js/alma.js
+++ src/Resources/public/js/alma.js
@@ -1,10 +1,12 @@
 $(function() {
-    $('.ui.form .item').each(function(i, e) {
-        $(e).find('.ui.radio').on('click', function() {
-            $('.ui.form .item .content .hidden').hide();
-            if ($(e).find('input[type=radio]').is(':checked')) {
-                $(e).find('.hidden').show();
-            }
+    if ($('.alma.ui').length > 0) {
+        $('.ui.form .item').each(function (i, e) {
+            $(e).find('.ui.radio').on('click', function () {
+                $('.ui.form .item .content .hidden').hide();
+                if ($(e).find('input[type=radio]').is(':checked')) {
+                    $(e).find('.hidden').show();
+                }
+            });
         });
-    });
+    }
 });
